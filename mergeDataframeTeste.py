b257d68f8af717cbd105f45fab0eabd9f0365e10
Luthiery Costa <luthierycosta@gmail.com>
2023-08-04 18:40:55 -0300

    Enfeitando o código - espaços, indentação, nome de variáveis

--- mergeDataframeTeste.py
+++ mergeDataframeTeste.py
@@ -10,24 +10,26 @@ for y in range(2008, 2022+1):
     # Obtém nome da pasta e do arquivo csv a ser lido
     folder = './Data_extracts/' + year
     [filename] = [file for file in listdir(folder) if file.endswith('_Data.csv')]
-    
-    # Cria dataframe a partir do arquivo
-    data = pd.read_csv(
-        folder+'/'+filename,
-        usecols = lambda col: col != 'Series Name',
-        na_values = '..'
-    )
-    # Remove as 5 últimas linhas (metadados)
-    data = data[:-5]
-    # Renomeia a coluna do ano para facilitar a manipulação
-    data = data.rename(columns={f'{year} [YR{year}]' : year})
-    # Cria atributo Year para diferenciar os registros desse dataframe dos outros anos
-    data['Year'] = y
-    
-    # Transpõe os valores de 'Series Code' como novas colunas, passando para 1481 colunas ao todo
-    data = data.pivot(index=['Country Name', 'Country Code', 'Year'], columns='Series Code', values=year).reset_index()
-    # Após o tratamento, insere dataframe no array
-    dataframes.append(data)
 
-# Ao final do loop, retorna a concatenação de todos os dataframes do array
+    # Cria dataframe a partir do arquivo
+    df = pd.read_csv(
+        folder+'/'+filename,
+        usecols = lambda col: col != 'Series Name', # Filtra colunas que não serão úteis no momento
+        na_values = '..'    # Interpreta '..' como NaN (valor nulo)
+    )
+
+    df = df[:-5]        # remove as 5 últimas linhas (metadados)
+    df = df.rename(columns={f'{year} [YR{year}]' : year})
+    df['Year'] = y
+
+    # Transpõe os valores de 'Series Code' como novas colunas, chegando a 1481 colunas ao todo
+    df = df.pivot(
+        index=['Country Name', 'Country Code', 'Year'],
+        columns='Series Code',
+        values=year
+    ).reset_index()
+    # Após o pré-processamento, insere dataframe no array
+    dataframes.append(df)
+
+# Ao final do loop, faz a concatenação de todos os dataframes do array
 finalData = pd.concat(dataframes).sort_values(['Country Name', 'Year']).reset_index(drop=True)
