109f1db94d37c127cf137900f480c0daca57f0cc
Luthiery Costa <luthierycosta@gmail.com>
2023-08-11 04:31:43 -0300

    Adicionado arquivo de parâmetros com filtro de países

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import csv_parser
+import params
 
-df = csv_parser.get_dataframes(2008, 2018)
+df = csv_parser.get_dataframes(params.initial_year, params.final_year)
 countries = csv_parser.get_countries_metadata()
 indicators = csv_parser.get_indicators_metadata()
 
 country_names = dict(countries['Table Name'])
 indicator_names = dict(indicators['Indicator Name'])
+
+# Filtra países mantendo apenas os que constam no arquivo params.py
+df = df[df['Country Name'].isin(params.countries)]
 
 count_nan = df.isna().sum()[3:]
 
@@ -25,10 +29,10 @@ count_nan_df['Series Name'] = pd.Series(indicator_names)
 count_nan_df.plot()
 
 #Filtra indicadores que possuem mais de 80% de valores não-nulos e remove todo o resto
-filtered_df = df.dropna(axis=1, thresh=0.8*len(df))
+filtered_df = df.dropna(axis=1, thresh=params.thresh*len(df))
 
 na_per_year = {}
-for year in range(2008, 2023):
+for year in range(params.initial_year, params.final_year+1):
     na_per_year[year] = df[df['Year'] == year].isna().sum(axis=1).median()
     
 # Gráfico que mostra a média de valores vazios por país, para cada ano
---
+++ params.py
@@ -0,0 +1,46 @@
+initial_year = 2008
+
+final_year = 2018
+
+thresh = 0.9
+
+countries = [
+    # América do Sul
+    'Brazil',
+    'Argentina',
+    'Bolivia',
+    'Chile',
+    'Colombia',
+    'Ecuador',
+    'Guyana',
+    'Paraguay',
+    'Peru',
+    'Suriname',
+    'Uruguay',
+    'Venezuela, RB',
+    # América do Norte
+    'Mexico',
+    'United States',
+    # Europa
+    'France',
+    'Germany',
+    'Portugal',
+    'Romania',
+    'Russian Federation',
+    'Spain',
+    # África
+    'Angola',
+    'Congo, Dem. Rep.',
+    'Egypt, Arab Rep.',
+    'Nigeria',
+    'South Africa',
+    # Ásia
+    'China',
+    'India',
+    'Indonesia',
+    'Japan',
+    'Korea, Rep.',
+    'Saudi Arabia',
+    # Oceania
+    'Australia'
+]
