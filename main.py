cd4d267c36ad76827bc5dc23196c93992f99e9d9
Luthiery Costa Cavalcante <luthierycosta@gmail.com>
2024-08-11 03:17:29 -0300

    melhoria: remoção de anos com muitos valores NaN

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ COUNTRIES_PATH = WORKSPACE_PATH + 'WDICountry.csv'
 INDICATORS_RESUMED_PATH = WORKSPACE_PATH + 'WDISeriesResumed.csv'
 INDICATORS_PATH = WORKSPACE_PATH + 'WDISeries.csv'
 
-NOT_NAN_FILTER = 0.8
-COUNTRIES_TO_DROP = 25
+NOT_NAN_FILTER = 0.7
+COUNTRIES_TO_DROP = 28
+YEARS_TO_DROP = 16
 TEST_SET_RATIO = 0.25
 FEATURES_TO_SELECT = 20
 
@@ -42,13 +43,14 @@ nan_per_indicator = pd.DataFrame(df.isna().sum()[3:]) \
 nan_per_indicator['Name'] = indicators['Indicator Name']
 # nan_per_indicator.plot()
 
-# Dataframe que mostra a qtd. de valores vazios por ano, contando todos os países
-nan_per_year = sorted(
-    [[year, df[df['Year'] == year].isna().sum().sum()]
-    for year in set(df['Year'])],
-    key=lambda arr: arr[0])
-nan_per_year = pd.DataFrame(nan_per_year, columns=['Year', 'NaN values'])
-nan_per_year.plot(x='Year', ylim=(0, 400000))
+# Dataframe que mostra a qtd. de valores vazios por ano, somando todos os países e indicadores
+nan_per_year = raw_df.isna().sum()[4:]
+nan_per_year.plot(xlabel='Year', ylim=(0, 400000))
+
+nan_per_year_b = raw_df.isna().sum()[4:]
+nan_per_year_b = nan_per_year_b.to_frame().reset_index()
+nan_per_year_b.columns = ['Year', 'NaN values']
+nan_per_year_b.plot(x='Year', ylim=(0, 400000))
 
 # Dataframe que mostra a qtd. de valores vazios para cada país, somando todos os anos
 nan_per_country = sorted([
@@ -56,10 +58,12 @@ nan_per_country = sorted([
     for country in set(df['Country Name'])],
     key=lambda arr: arr[1], reverse=True)
 nan_per_country = pd.DataFrame(nan_per_country, columns=['Country Name', 'NaN values'])
-# nan_per_country.plot()
 
 
 ### Pré-processamento
+
+# Remove os anos que possuem mais valores vazios, conforme parâmetro
+df = df[~df['Year'].isin(nan_per_year.nlargest(YEARS_TO_DROP).index.astype(int))]
 
 # Remove os países que possuem mais valores vazios, conforme parâmetro
 df = df[~df['Country Name'].isin(nan_per_country.head(COUNTRIES_TO_DROP)['Country Name'])]
@@ -68,13 +72,20 @@ df = df[~df['Country Name'].isin(nan_per_country.head(COUNTRIES_TO_DROP)['Countr
 df = df.dropna(axis=1, thresh=NOT_NAN_FILTER*len(df))
 
 # Exlui registros que possuem a variável "crescimento do PIB" (o alvo do modelo) vazia
-[gdp_growth_code] = indicators.query("`Indicator Name` == 'GDP growth (annual %)'")['Series Code']
+[gdp_growth_code] = indicators.query("`Indicator Name` == 'GDP growth (annual %)'").index
 df = df.dropna(subset=[gdp_growth_code])
 
 
 ### Processamento dos conjuntos de teste e treinamento
 
+# TODO: REMOVER ESSES INDICADORES DO CONJUNTO DE TREINAMENTO
+gdp_indicators = indicators[indicators['Indicator Name'].str.contains("GDP")]
+gni_indicators = indicators[indicators['Indicator Name'].str.contains("GNI")]
+
+gdp_indicators.to_csv(WORKSPACE_PATH + 'GDPindicators.csv', columns=['Indicator Name'])
+
 # Separa as variáveis de entrada (X) e variável alvo (y)
+# e remove as variáveis relacionadas ao PIB
 X = df.drop(columns=['Country Name', 'Country Code', 'Year'])
 y = df[gdp_growth_code]
 
