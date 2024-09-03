8eb233aec1de7bb8aace8f7d720b48e737d7d847
Luthiery Costa Cavalcante <luthierycosta@gmail.com>
2024-09-02 21:07:19 -0300

    Material overleffffffff

---
+++ material_overleaf/parametros_pre_proc.py
@@ -0,0 +1,3 @@
+YEARS_TO_DROP = 16                  # 1/4 do total de anos
+COUNTRIES_TO_DROP = 28              # aprox. 10% dos 266 países e regiões
+INDICATORS_NOT_NAN_THRESHOLD = 0.6  # exclui todo indicador com + de 40% valores nulos
